67c47371332468d28b439bec99dad16e21e2d3fb
T Bird <tompcapers@gmail.com>
2021-06-15 17:53:20 -0700

    updated index js for prompts

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 //inquire asks for manager info
 
 //ask if user wishes to fill out other employee positions
@@ -13,14 +16,52 @@
         // write new html file into dist folder
         // 
 
-askForEmployee() {
-    return inquirer
+
+    inquirer
         .prompt([
         {
             type: "input",
             name: "choice",
-            message: "Please enter the anager's name.",
+            message: "Please enter the manager's name.",
                      
-        }
+        },
+        {
+            type: "input",
+            name: "choice",
+            message: "Please enter the employee ID.",
+                     
+        },
+        {
+            type: "input",
+            name: "choice",
+            message: "Please enter the email address.",
+                     
+        },
+        {
+            type: "input",
+            name: "choice",
+            message: "Please enter the office number.",
+                     
+        },
+        {
+            type: 'checkbox',
+            message: 'Do you wish to add another employee?',
+            name: 'role',
+            choices: ['Yes', 'No'],
+          },
+
+        {
+            type: 'checkbox',
+            message: 'What is the employees role?',
+            name: 'role',
+            choices: ['Engineer', 'Intern'],
+          },
         ])
-    }
+        .then((data) => {
+            const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+        
+            fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+              err ? console.log(err) : console.log('Success!')
+            );
+          });
+    
